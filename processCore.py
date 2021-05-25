d10840a6c989b4f772775057ebe06a2c0fe373bb
nicolas-nogueira-dev <71648696+RacoonLover@users.noreply.github.com>
2021-05-25 16:21:07 +0200

    Work Update

--- processCore.py
+++ processCore.py
@@ -30,7 +30,8 @@ class ProcessCore(Core):
     def processCleanTokens(self, settings):
         print('-> Getting the dataset...')
         start_time = time.time()
-        dataset = self.getRawDataset(self.folderPath+settings['filePath'], settings['type'], settings)
+        filePath = self.folderPath+settings['filePath']
+        dataset = self.getRawDataset(filePath, settings)
         print('-> %s seconds for get the dataset' % (time.time() - start_time))
         print('')
         print('-> Getting the tokens...')
@@ -41,15 +42,27 @@ class ProcessCore(Core):
             start_time_tmp = time.time()
             tokens[sentiment] = self.getSentimentItems(dataset, settings['sentimentText'][sentiment])
             print(f'-> {time.time() - start_time_tmp} seconds for get the {sentiment} tokens')
-        print('-> %s seconds for get the positive/negative tokens' % (time.time() - start_time))
+        print('-> %s seconds for get the tokens' % (time.time() - start_time))
 
-        # Define the stop words
         stop_words = stopwords.words(settings['stopWord'])
 
+        print('')
+        print('-> Cleaning the tokens...')
+        cleaned_tokens = {}
+        start_time = time.time()
+        for sentiment in self.sentiments:
+            print(f'-> Cleaning the {sentiment} tokens...')
+            start_time_tmp = time.time()
+            cleaned_tokens[sentiment] = self.getCleanTokens(tokens[sentiment], stop_words)
+            print(f'-> {time.time() - start_time_tmp} seconds for clean the {sentiment} tokens')
+        print('-> %s seconds for clean the tokens' % (time.time() - start_time))
+
+        for sentiment in self.sentiments:
+            self.saveCleanData(cleaned_tokens[sentiment], sentiment, str(sentiment) + '_cleaned_tokens')
 
 main = ProcessCore('dataset3/', ['positive','negative','neutral'])
 
-main.processCleanTokens({'filePath':'dataset4.csv',
+main.processCleanTokens({'filePath':'dataset3.csv',
                          'type':'csv',
                          'encoding':'utf8',
                          'delimiter':',',
