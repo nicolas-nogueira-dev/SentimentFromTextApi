909ea3a7b3877b99e5fdabf507396dacb7b2ab38
nicolas-nogueira-dev <71648696+RacoonLover@users.noreply.github.com>
2021-05-26 17:13:25 +0200

    Work Update

--- server.py
+++ server.py
@@ -4,16 +4,32 @@ from core import *
 from classifierCore import *
 from processCore import *
 
+datasetInfos = {'dataset1':{'name':'dataset1',
+                            'sentiments':['positive',
+                                          'negative',
+                                          'neutral']},
+                'dataset2':{'name':'dataset2',
+                            'sentiments':['positive',
+                                          'negative']},
+                'dataset3':{'name':'dataset3',
+                            'sentiments':['positive',
+                                          'negative']},
+                'dataset4':{'name':'dataset4',
+                            'sentiments':['positive',
+                                          'negative']},
+                'dataset5':{'name':'dataset5',
+                            'sentiments':['positive',
+                                          'negative']},
+}
+
+
 app = Sanic('TextToSentiment')
 useDataset = 'dataset5'
-sentiments = ['positive', 'negative']
 classifierPath = 'classifier.pickle'
 
-classifierCore = ClassifierCore(useDataset,sentiments)
+classifierCore = ClassifierCore(useDataset,datasetInfos[useDataset]['sentiments'])
 
 classifier = classifierCore.trainDataValidator()
-
-classifierCore.textToSentiment(classifier, 'I love chocolat')
 
 async def home(request):
     with open('pages/home.html', 'r', encoding='utf8') as file:
@@ -37,15 +53,12 @@ async def dataset_handler(request):
 
 async def choice_handler(request):
     global useDataset
-    global sentiments
     useDataset = str(request.form.get('datasetChoice'))
-    sentiments = str(request.form.get('sentimentsChoice')).split(",")
     return response.redirect('/')
 
 async def debug_handler(request):
     global useDataset
-    global sentiments
-    infos = {'useDataset':useDataset,'sentiments':sentiments}
+    infos = {'useDataset':useDataset,'sentiments':datasetInfos[useDataset]['sentiments']}
     return response.json(infos)
 
 app.add_route(home, '/')
